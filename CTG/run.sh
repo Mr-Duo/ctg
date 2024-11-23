ad256529621b58db1e4bf216cb0408dcd81008b0
Mr-Duo <duong.nd215336@sis.hust.edu.vn>
2024-11-23 19:26:28 +0700

    a

--- CTG/run.sh
+++ CTG/run.sh
@@ -5,8 +5,8 @@ file=$2
 begin=$3
 end=$4
 
-python parse_impacted_dependency_lines.py --project $prj --file $file --begin $begin --end $end
-python generate_jit_vul_triggering_commit_data.py --project $prj --file $file --begin $begin --end $end
-python parser_cpg_data.py --project $prj --file $file
-python generate_ctg_graph.py --project $prj --file $file
-python Main_Trim_CTG.py --data "$file"_ctg.csv
+/usr/bin/python3.7 python parse_impacted_dependency_lines.py --project $prj --file $file --begin $begin --end $end
+/usr/bin/python3.7 python generate_jit_vul_triggering_commit_data.py --project $prj --file $file --begin $begin --end $end
+/usr/bin/python3.7 python parser_cpg_data.py --project $prj --file $file
+/usr/bin/python3.7 python generate_ctg_graph.py --project $prj --file $file
+/usr/bin/python3.7 python Main_Trim_CTG.py --data "$file"_ctg.csv
