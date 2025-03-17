be6c7c4ac8e50549d8a62d72feab063a9db7eea6
Mr-Duo <103053799+Mr-Duo@users.noreply.github.com>
2025-03-17 21:47:51 +0700

    Update run.sh

--- CTG/run.sh
+++ CTG/run.sh
@@ -5,7 +5,7 @@ file=$2
 begin=$3
 end=$4
 
-/usr/bin/python3.7 parse_impacted_dependency_lines.py --project $prj --file $file --begin $begin --end $end
+#/usr/bin/python3.7 parse_impacted_dependency_lines.py --project $prj --file $file --begin $begin --end $end
 /usr/bin/python3.7 generate_jit_vul_triggering_commit_data.py --project $prj --file $file --begin $begin --end $end
 /usr/bin/python3.7 parser_cpg_data.py --project $prj --file $file
 /usr/bin/python3.7 generate_ctg_graph.py --project $prj --file $file
